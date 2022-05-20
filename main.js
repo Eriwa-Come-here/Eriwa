bb5135baa50d314211545361886e130772f9d03e
Duma <x__x73@naver.com>
2022-05-20 13:20:10 +0900

    add main.js / errController.js

---
+++ main.js
@@ -0,0 +1,32 @@
+const port = 80,
+	express = require("express"),
+    app=express(),
+    layouts = require("express-ejs-layouts"),
+    errorController = require("./controllers/errorController");
+
+app.set("port", process.env.PORT || 80);
+app.set("view engine", "ejs");
+
+app.use(layouts);
+app.use(express.static("public"));
+
+app.use(
+    express.urlencoded({
+        extended:false
+    })
+);
+app.use(express.json());
+
+//ejs 파일 렌더링 추가
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+//errorController 추가
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+//
+app.listen(app.get("port"), () =>{
+    console.log(`Server running at http://localhost:${app.get("port")}`)
+});
