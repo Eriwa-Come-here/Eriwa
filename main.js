20eab23f7fb75987b0febba3f02425af10f1100a
minha-ss <alsgk6989@gmail.com>
2022-05-23 16:10:18 +0900

    add postController to main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const port = 80,
-	express = require("express"),
-    app=express(),
-    layouts = require("express-ejs-layouts"),
-    errorController = require("./controllers/errorController");
+  express = require("express"),
+  app = express(),
+  layouts = require("express-ejs-layouts"),
+  errorController = require("./controllers/errorController");
 
 app.set("port", process.env.PORT || 80);
 app.set("view engine", "ejs");
@@ -11,22 +11,25 @@ app.use(layouts);
 app.use(express.static("public"));
 
 app.use(
-    express.urlencoded({
-        extended:false
-    })
+  express.urlencoded({
+    extended: false,
+  })
 );
 app.use(express.json());
 
 //ejs 파일 렌더링 추가
-app.get("/", (req, res)=>{
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
+
+//postController추가
+app.get("/board/post-writing", postController.showPostWriting);
 
 //errorController 추가
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
 
 //
-app.listen(app.get("port"), () =>{
-    console.log(`Server running at http://localhost:${app.get("port")}`)
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
 });
