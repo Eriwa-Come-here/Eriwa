5006c60b1123eab2eac57e1c02c7424ef70760ae
minha-ss <alsgk6989@gmail.com>
2022-05-23 22:11:00 +0900

    modify main.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,14 @@
 const httpStatus = require("http-status-codes");
 
 exports.pageNotFoundError = (req, res) => {
-    let errorCode = httpStatus.NOT_FOUND;
-    res.status(errorCode);
-    res.render("error");
-}
+  let errorCode = httpStatus.NOT_FOUND;
+  res.status(errorCode);
+  res.render("error");
+};
 
 exports.internalServerError = (error, req, res, next) => {
-    let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
-    console.log(`ERROR occured: ${error.stack}`);
-    res.status(errorCode);
-    res.send(`${errorCode} | Sorry, our application is taking a nap!`);
-}
+  let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+  console.log(`ERROR occured: ${error.stack}`);
+  res.status(errorCode);
+  res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+};
--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const port = 80,
   errorController = require("./controllers/errorController"),
   homeController = require("./controllers/homeController"),
   postController = require("./controllers/postController"),
-  adminController = require("./controllers/adminController");
+  adminController = require("./controllers/adminController"),
+  mypageController = require("./controllers/mypageController"),
+  loginController = require("./controllers/loginController");
 
 app.set("port", process.env.PORT || 80);
 app.set("view engine", "ejs");
@@ -37,6 +39,7 @@ app.get("/search", homeController.showDetailSearch);
 app.get("/qna", homeController.showQna);
 app.get("/recommend", homeController.showRecommend);
 app.get("/service-intro", homeController.showServiceIntro);
+app.get("/notice", homeController.showNotice);
 
 //postController추가
 app.get("/board/post-writing", postController.showPostWriting);
@@ -48,6 +51,17 @@ app.get("/admin-post", adminController.showAdminPost);
 app.get("/admin-analysis", adminController.showAdminAnalysis);
 app.get("/admin-qna", adminController.showAdminQna);
 app.get("/admin-qna/admin-qna-response", adminController.showAdminQnaResponse);
+
+//mypageController 추가
+app.get("/mypage_good_list", mypageController.mypageGood);
+app.get("/mypage_reply_list", mypageController.mypageReply);
+app.get("/mypage-write-list", mypageController.mypageWrite);
+app.get("/chat-list", mypageController.chatList);
+app.get("/chat", mypageController.chatStory);
+
+//loginController 추가
+app.get("/login", loginController.login);
+app.get("/signup", loginController.signup);
 
 //errorController 추가
 app.use(errorController.pageNotFoundError);
