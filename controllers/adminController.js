55fd440f0108f9e88a46b60faa6b9c83eba57b7c
minha-ss <alsgk6989@gmail.com>
2022-05-28 19:43:56 +0900

    modify adminController.js & index.js & Create qna.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,19 +1,24 @@
+//회원관리
 exports.showAdminMember = (req, res) => {
   res.render("admin-member");
 };
 
+//게시물관리
 exports.showAdminPost = (req, res) => {
   res.render("admin-post");
 };
 
+//통계분석
 exports.showAdminAnalysis = (req, res) => {
   res.render("admin-analysis");
 };
 
+//문의내역
 exports.showAdminQna = (req, res) => {
   res.render("admin-qna");
 };
 
+//문의글답변
 exports.showAdminQnaResponse = (req, res) => {
   res.render("admin-qna-response");
 };
--- models/index.js
+++ models/index.js
@@ -1,13 +1,21 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 // db.subscriber = require("./subscriber.js")(sequelize, Sequelize);
+
+//qna DB 연결
+db.qna = require("./qna.js")(sequelize, Sequelize);
 
 module.exports = db;
