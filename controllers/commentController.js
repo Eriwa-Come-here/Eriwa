48305e8c4e8be184aee1b2ab9884aa08fe9ecf74
jay <orzero999@gmail.com>
2022-06-12 18:01:04 +0900

    show nickname instead of id

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -14,7 +14,7 @@ module.exports = {
       });
       await sequelize.query("INSERT INTO `event`(`user_id`, `event_type`, `event_author`, `event_title`, `event_content`, `url_address`, `event_date`) VALUES (?, ?, ?, ?, ?, ?, ?)", {
         type: sequelize.QueryTypes.INSERT,
-        replacements: [req.body.post_author, '댓글', req.body.comment_author, req.body.post_title, req.body.comment_content, `/board/post-view/${req.params.post_id}`, date]
+        replacements: [req.body.post_author, '댓글', req.body.comment_author_nickname, req.body.post_title, req.body.comment_content, `/board/post-view/${req.params.post_id}`, date]
       });
       res.redirect("/board/post-view/" + req.params.post_id);
     } catch (err) {
