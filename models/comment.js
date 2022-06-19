1ae931da64f1ffed2f0029fe04477e2237874edc
minha-ss <alsgk6989@gmail.com>
2022-06-19 15:48:00 +0900

    modify comment.js index.js user.js

--- models/comment.js
+++ models/comment.js
@@ -2,34 +2,7 @@
 module.exports = (sequelize, Sequelize) => {
   const User = require("./user")(sequelize, Sequelize);
   const Post = require("./post")(sequelize, Sequelize);
-  class Comment extends Sequelize.Model {
-    // static async findByPkAndUpdate(id, params) {
-    //   try {
-    //     let comment = await Comment.findByPk(id);
-    //     if (comment) {
-    //       comment = await Comment.update(params, {
-    //         where: { id: id },
-    //       });
-    //     }
-    //     return comment;
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // }
-    // static async findByPkAndRemove(id) {
-    //   try {
-    //     let comment = await Comment.findByPk(id);
-    //     if (comment) {
-    //       comment = await Comment.destroy(params, {
-    //         where: { id: id },
-    //       });
-    //     }
-    //     return comment;
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // }
-  }
+  class Comment extends Sequelize.Model {}
   Comment.init(
     {
       post_id: {
--- models/index.js
+++ models/index.js
@@ -18,18 +18,7 @@ db.post = require("./post.js")(sequelize, Sequelize);
 db.qna = require("./qna.js")(sequelize, Sequelize);
 db.recommend = require("./recommend.js")(sequelize, Sequelize);
 db.comment = require("./comment.js")(sequelize, Sequelize);
-
-//관계
-/*
-db.user.hasMany(db.post);
-db.post.belongsTo(db.user);
-db.user.hasMany(db.comment);
-db.comment.belongsTo(db.user);
-
-db.post.hasMany(db.comment);
-db.comment.belongsTo(db.post);
-
-db.user.belongsToMany(db.post, {through: "Recommend"});
-db.post.belongsToMany(db.user, {through: "Recommend"});*/
+db.event = require("./event.js")(sequelize, Sequelize);
+db.note = require("./note.js")(sequelize, Sequelize);
 
 module.exports = db;
